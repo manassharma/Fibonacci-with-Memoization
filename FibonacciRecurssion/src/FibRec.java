a54faed39298a5b9070971d0f06aae0acd409cc3
Manas Sharma <manas_sharma@msn.com>
2016-02-18 21:19:01 -0700

    Changes to main

--- FibonacciRecurssion/src/FibRec.java
+++ FibonacciRecurssion/src/FibRec.java
@@ -6,31 +6,30 @@ public class FibRec {
 	static int answer;
 
 	static Map<Integer, Integer> dict = new HashMap<Integer, Integer>();
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		String userInput = in.next();
 		Integer userInt = Integer.parseInt(userInput);
-		int temp = (int)userInt;
+		int temp = (int) userInt;
 		int sol = findFib(temp);
-		System.out.println("Fibonacci sequence is = "+sol);
+		System.out.println("Fibonacci sequence is = " + sol);
 	}
-	
-	public static int findFib(int n){
-		if(dict.containsKey(n)){
+
+	private static int findFib(int n) {
+		if (dict.containsKey(n)) {
 			answer = dict.get(n);
 			return answer;
 		}
-		if(n<=2){
+		if (n <= 2) {
 			return 1;
-		}
-		else{
-			answer = findFib(n-1) + findFib(n-2);
+		} else {
+			answer = findFib(n - 1) + findFib(n - 2);
 		}
 		Integer mapKey = n;
 		Integer mapValue = answer;
 		dict.put(mapKey, mapValue);
-		
+
 		return answer;
 	}
 }
